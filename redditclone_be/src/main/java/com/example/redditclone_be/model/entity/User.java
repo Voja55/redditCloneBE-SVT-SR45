1fd055d2eadd4e7391a87dd3dff62415a112732e
Voja55 <vojislav.culibrk@mail.ru>
2022-06-19 03:03:30 +0200

    H2 baza setup

--- redditclone_be/src/main/java/com/example/redditclone_be/model/entity/User.java
+++ redditclone_be/src/main/java/com/example/redditclone_be/model/entity/User.java
@@ -3,10 +3,8 @@ package com.example.redditclone_be.model.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 @Getter
@@ -16,4 +14,25 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private  String email;
+
+    @Column(nullable = false)
+    private String avatar;
+
+    @Column(nullable = false)
+    private LocalDate regDate;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false, unique = true)
+    private  String displayName;
 }
